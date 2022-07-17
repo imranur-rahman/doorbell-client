17f4c3e61c321352ea1077958576c2afa4aa6366
Imranur Rahman <ir.shimul@gmail.com>
2022-07-18 00:53:05 +0600

    Added addEdge related codes.
    1. Created prompt.xml
    2. Updated SecondFragment.java with new button for prompt.
    3. Handler for addEdge button and basic logics.
    4. Added logics for the AlertDialogBuilder in prompt.xml

--- app/src/main/java/edu/ncsu/doorbellclient/SecondFragment.java
+++ app/src/main/java/edu/ncsu/doorbellclient/SecondFragment.java
@@ -1,9 +1,16 @@
 package edu.ncsu.doorbellclient;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -34,6 +41,59 @@ public class SecondFragment extends Fragment {
             public void onClick(View view) {
                 NavHostFragment.findNavController(SecondFragment.this)
                         .navigate(R.id.action_SecondFragment_to_FirstFragment);
+            }
+        });
+
+        // https://developer.android.com/topic/libraries/view-binding
+        // https://mkyong.com/android/android-prompt-user-input-dialog-example/
+        binding.addEdge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Context context = getContext();
+                // get prompts.xml view
+                LayoutInflater li = LayoutInflater.from(context);
+                View promptsView = li.inflate(R.layout.prompt, null);
+
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                        context);
+
+                // set prompts.xml to alertdialog builder
+                alertDialogBuilder.setView(promptsView);
+
+                final EditText userInput1 = (EditText) promptsView
+                        .findViewById(R.id.editTextDialogUserInput1);
+                final EditText userInput2 = (EditText) promptsView
+                        .findViewById(R.id.editTextDialogUserInput2);
+
+                // set dialog message
+                alertDialogBuilder
+                        .setCancelable(false)
+                        .setPositiveButton("OK",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,int id) {
+                                        // get user input and set it to result
+                                        // edit text
+                                        // result.setText(userInput.getText());
+                                        Toast.makeText(context,
+                                                userInput1.getText().toString() + userInput2.getText().toString(),
+                                                Toast.LENGTH_LONG); // not working donno why
+                                        Log.d(this.getClass().getSimpleName(),
+                                                userInput1.getText().toString() + userInput2.getText().toString()); // working
+                                        // Need to add logic here to update the firebase database
+                                    }
+                                })
+                        .setNegativeButton("Cancel",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+
+                // create alert dialog
+                AlertDialog alertDialog = alertDialogBuilder.create();
+
+                // show it
+                alertDialog.show();
             }
         });
     }
