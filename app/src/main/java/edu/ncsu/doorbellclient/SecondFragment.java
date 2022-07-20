736c00a63ab97bb17c230ca6fd5f564b4f25b473
Imranur Rahman <ir.shimul@gmail.com>
2022-07-20 13:34:01 +0600

    Experimental task, trying to add graphview to view the relationship. Not working at this moment

--- app/src/main/java/edu/ncsu/doorbellclient/SecondFragment.java
+++ app/src/main/java/edu/ncsu/doorbellclient/SecondFragment.java
@@ -3,8 +3,11 @@ package edu.ncsu.doorbellclient;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.graphics.CornerPathEffect;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,14 +16,28 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import dev.bandb.graphview.AbstractGraphAdapter;
+import dev.bandb.graphview.decoration.edge.ArrowEdgeDecoration;
+import dev.bandb.graphview.graph.Graph;
+import dev.bandb.graphview.layouts.layered.SugiyamaConfiguration;
+import dev.bandb.graphview.layouts.layered.SugiyamaLayoutManager;
+import dev.bandb.graphview.layouts.tree.BuchheimWalkerConfiguration;
+import dev.bandb.graphview.layouts.tree.BuchheimWalkerLayoutManager;
+import dev.bandb.graphview.layouts.tree.TreeEdgeDecoration;
 import edu.ncsu.doorbellclient.databinding.FragmentSecondBinding;
 
 public class SecondFragment extends Fragment {
 
     private FragmentSecondBinding binding;
+    private RecyclerView mRelationRecycler;
+    //private RelationListAdapter mRelationAdapter;
+    private AbstractGraphAdapter graphAdapter;
 
     @Override
     public View onCreateView(
@@ -29,12 +46,53 @@ public class SecondFragment extends Fragment {
     ) {
 
         binding = FragmentSecondBinding.inflate(inflater, container, false);
-        return binding.getRoot();
+        View root = binding.getRoot();
 
+        mRelationRecycler = binding.recycler;
+        mRelationRecycler.setLayoutManager(new LinearLayoutManager(binding.getRoot().getContext()));
+        mRelationRecycler.setNestedScrollingEnabled(false);
+        
+        initGraph();
+        
+        return root;
+    }
+
+    private void initGraph() {
+        SugiyamaConfiguration configuration = new SugiyamaConfiguration.Builder()
+                .setNodeSeparation(100)
+                .setLevelSeparation(100)
+                .build();
+        binding.recycler.setLayoutManager(new SugiyamaLayoutManager(binding.getRoot().getContext(), configuration));
+        Paint edgeStyle = new Paint(Paint.ANTI_ALIAS_FLAG);
+        TypedValue typedValue = new TypedValue();
+        binding.getRoot().getContext().getTheme().resolveAttribute(R.attr.colorSecondary, typedValue, true);
+        int color = ContextCompat.getColor(binding.getRoot().getContext(), typedValue.resourceId);
+        edgeStyle.setColor(color);
+        edgeStyle.setStrokeWidth(5f);
+        edgeStyle.setStyle(Paint.Style.STROKE);
+        edgeStyle.setStrokeJoin(Paint.Join.ROUND);
+        edgeStyle.setPathEffect(new CornerPathEffect(10f));
+        binding.addEdge.addItemDecoration(new ArrowEdgeDecoration(edgeStyle));
+
+        graphAdapter = new AbstractGraphAdapter<NodeHolder>() {
+            @Override
+            public NodeHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                NodeBinding binding = NodeBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
+                return new NodeHolder(binding);
+            }
+
+            @Override
+            public void onBindViewHolder(NodeHolder holder, int position) {
+                holder.set((EntityInfo) getNodeData(position));
+            }
+        };
+        binding.graphView.setAdapter(graphAdapter);
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        Context context = getContext();
 
         binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,7 +107,6 @@ public class SecondFragment extends Fragment {
         binding.addEdge.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Context context = getContext();
                 // get prompts.xml view
                 LayoutInflater li = LayoutInflater.from(context);
                 View promptsView = li.inflate(R.layout.prompt, null);
@@ -99,6 +156,34 @@ public class SecondFragment extends Fragment {
                 alertDialog.show();
             }
         });
+
+
+
+        // setup the graphview
+
+
+        // 1. Set a layout manager of the ones described above that the RecyclerView will use.
+//        BuchheimWalkerConfiguration configuration = new BuchheimWalkerConfiguration.Builder()
+//                                .setSiblingSeparation(100)
+//                                .setLevelSeparation(100)
+//                                .setSubtreeSeparation(100)
+//                                .setOrientation(BuchheimWalkerConfiguration.ORIENTATION_TOP_BOTTOM)
+//                                .build();
+        SugiyamaConfiguration configuration = new SugiyamaConfiguration.Builder()
+//                                .setSiblingSeparation(100)
+                                .setLevelSeparation(100)
+//                                .setSubtreeSeparation(100)
+//                                .setOrientation(BuchheimWalkerConfiguration.ORIENTATION_TOP_BOTTOM)
+                                .build();
+
+        BuchheimWalkerLayoutManager buchheimWalkerLayoutManager = new BuchheimWalkerLayoutManager(context, configuration);
+        binding.recycler.setLayoutManager(new SugiyamaLayoutManager(context, configuration));
+        //binding.recycler.setLayoutManager(new Layoutmanager);
+
+        // 2. Attach item decorations to draw edges
+        binding.recycler.addItemDecoration(new TreeEdgeDecoration());
+
+        new Graph();
     }
 
     @Override
